98adc38063f281778f667eead994238c2d7bff6b
Aashish <aashish@aashishjoshi.co.in>
2022-02-02 20:34:04 +0530

    ver 0.1.4

--- Readme.md
+++ Readme.md
@@ -4,25 +4,27 @@
     <img src="https://dl.dropboxusercontent.com/s/hn4o0v378od1aoo/logo_white_background.png" alt="Logo" width="80" height="80">
   </a>
 
-<h3 align="center">Sonetel API</h3>
+<h3 align="center">Sonetel API Python Wrapper</h3>
 
 <p align="center">
-    A simple wrapper for using Sonetel's REST API endpoints.
+    A simple Python wrapper for using Sonetel's REST API endpoints.
     <br />
     <br />
-    <a href="https://sonetel.com/en/developer/" target="_blank">Sonetel Developer Home</a>
+    <a href="https://sonetel.com/en/developer/">Sonetel Developer Home</a>
     .
-    <a href="https://sonetel.com/en/developer/api-documentation/" target="_blank">API Documentation</a>
+    <a href="https://sonetel.com/en/developer/api-documentation/">API Documentation</a>
+    .
+    <a href="https://app.sonetel.com/register?tag=api-developer&simple=true">Get Free Account</a>
   </p>
 </div>
 
 ## Getting Started
 
-### Get a Sonetel account
-
-Sign up for a free account from <a href="https://app.sonetel.com/register?tag=api-developer&simple=true">sonetel.com</a>.
+To use Sonetel's APIs you first need to sign up for a free account. Get a free account from <a href="https://app.sonetel.com/register?tag=api-developer&simple=true">sonetel.com</a>.
 
 ### Installation
+
+Use PIP to install the package.
 
 `pip install sonetel`
 
@@ -33,9 +35,11 @@ Sign up for a free account from <a href="https://app.sonetel.com/register?tag=ap
 Here's a list of functions available in different packages.
 
 #### Account
-- `account.get_token()` - Get an access token to authenticate API requests. 
-- `account.get_accountid()` - Get your Sonetel account ID
-- `account.get_balance()` - Get your Sonetel prepaid balance
+
+- `account.token()` - Get an access token to authenticate API requests. Can be used to refresh an existing access token as well.
+- `account.get_account()` - Get information about your Sonetel account such as the account ID, prepaid balance, etc
+- `account.get_balance()` - Get your Sonetel prepaid balance.
+- `account.get_all_users` - Get a list of all users in your Sonetel account.
 
 #### Subscription
 - `subscription.buy_number()` - Buy a phone number
@@ -47,23 +51,19 @@ Here's a list of functions available in different packages.
 ```python
 from sonetel import account
 
-api_base_url = 'https://public-api.sonetel.com'
-
 # get API access token
-access_token = account.get_token(
+token = account.token(
     username = "YOUR_SONETEL_USERNAME",
-    password='YOUR_SONETEL_PASSWORD',
-    auth_url='https://api.sonetel.com/SonetelAuth/beta/oauth/token')
+    password = "YOUR_SONETEL_PASSWORD")
 
 # Print the Sonetel account ID and current account balance.
-account_id = account.get_accountid(
-    token=access_token,
-    base_url=api_base_url)
+account_id = account.get_account(
+    token=token["access_token"],
+    return_only_accountid=True)
 
 balance = account.get_balance(
-    token=access_token, 
-    currency=True,
-    base_url = api_base_url)
+    token=token["access_token"], 
+    currency=True)
 
 print(f"Your account ID is {account_id} and your prepaid balance is {balance}.")
 ```
@@ -72,30 +72,45 @@ print(f"Your account ID is {account_id} and your prepaid balance is {balance}.")
 ```python
 from sonetel import account, subscription
 
-api_base_url = "https://public-api.sonetel.com"
-
 # get API access token
-access_token = account.get_token(
-    username = 'YOUR_SONETEL_USERNAME',
-    password='YOUR_SONETEL_PASSWORD',
-    auth_url='https://api.sonetel.com/SonetelAuth/beta/oauth/token')
+token = account.token(
+    username = "YOUR_SONETEL_USERNAME",
+    password = "YOUR_SONETEL_PASSWORD")
 
 # Get Sonetel account ID
-account_id = account.get_accountid(
-    token=access_token,
-    base_url=api_base_url)
+account_id = account.get_account(
+    token=token["access_token"],
+    return_only_accountid=True)
 
 # Get the list of phone numbers
 numList = subscription.list_all_numbers(
-    token=access_token,
-    base_url=api_base_url,
-    accountid=account_id
-)
+    token=token["access_token"],
+    accountid=account_id)
 
 print(f"Phone number list for account {account_id}:\n")
 
-for entry in numList["response"]:
-    print(entry['phnum'])
+for entry in numList:
+    print(entry["phnum"])
+```
+
+### Storing your credentials
+
+**IMPORTANT** It is critical that you keep your Sonetel login credentials safe in order to avoid any misuse of your account.
+
+For this reason, we don't recommend hard coding them into scripts. You can add them to your operating system's environment variables and use Python's OS module to fetch the details.
+
+Assuming the username and password are stored in environment variables named `sonetelUserName` and `sonetelPassword` respectively, here's how you can access them from a script:
+
+```python
+import os
+from sonetel import account
+
+user = os.environ.get('sonetelUserName')
+pswd = os.environ.get('sonetelPassword')
+
+token = account.token(
+    username = user,
+    password=pswd)
 ```
 
 ## Help
--- src/sonetel/account.py
+++ src/sonetel/account.py
@@ -1,26 +1,39 @@
 import requests
 
 # Generate an access token
-def get_token(username: str = None, password: str = None, auth_url: str = None) -> str:
+def token(username: str, password: str, auth_url: str = 'https://api.sonetel.com/SonetelAuth/beta/oauth/token',
+          refresh: str = "no", grant_type: str = "password", refresh_token: str = None) -> dict:
     """
     Create an API access token from the user's Sonetel email address and password.
+    Optionally, generate a refresh token as well. Set the `grant_type` to `refresh_token` to refresh an
+    existing access token.
+    More information is available at https://docs.sonetel.com/docs/sonetel-documentation/YXBpOjExMzI3NDM3-authentication
+
     :param username: Your Sonetel email address.
     :param password: Your Sonetel password.
     :param auth_url: The API URL for generating access token.
+    :param refresh: Optional. Flag to control whether a refresh token is included in the response. Defaults to 'no'
+    :param grant_type: Optional. The OAuth2 grant type. Defaults to 'password'
+    :parameter refresh_token: Optional. Pass the `refresh_token` in this field to generate a new access_token.
     :return: Returns the access token.
     """
 
-    if username is None or username == '':
-        raise ValueError("username is required")
-    elif password is None or password == '':
-        raise ValueError("password is required")
-    elif auth_url is None or auth_url == '':
-        raise ValueError("auth_url is required")
+    if grant_type == 'refresh_token' and refresh_token is None:
+        raise ValueError("A refresh_token is needed when grant type is refresh_token.")
 
-    body = f"grant_type=password&username={username}&password={password}&refresh=no"
+    # Prepare the request body.
+    body = f"grant_type={grant_type}&username={username}&password={password}&refresh={refresh}"
+
+    # Add the refresh token to the request body if passed to the function
+    if refresh_token is not None:
+        body += f"&refresh_token={refresh_token}"
+
     auth = ('sonetel-web', 'sonetel-web')
+
+    # Prepare the request headers
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
+    # Send the request
     r = requests.post(
         url=auth_url,
         data=body,
@@ -28,75 +41,111 @@ def get_token(username: str = None, password: str = None, auth_url: str = None) 
         auth=auth
     )
 
+    # Check the response and handle accordingly.
     if r.status_code == requests.codes.ok:
-        data = r.json()
-        if 'access_token' in data.keys():
-            return data['access_token']
-        else:
-            return None
+        return r.json()
     else:
         print(r.json())
         r.raise_for_status()
 
-# Get the account ID
-def get_accountid(token: str = None, base_url: str = None):
+
+# Get information about the Sonetel account
+def get_account(token: str, base_url: str = 'https://public-api.sonetel.com', return_only_accountid: bool = False):
 
     """
-    Get the Sonetel account ID of a user.
+    Get information about Sonetel account such as the account ID, prepaid balance, currency, country, etc.
+    More information is available at https://docs.sonetel.com/docs/sonetel-documentation/b3A6MTUyNTkwNTM-get-your-account-information
 
     :param token: API access token generated using the token() function.
     :param base_url: The API base URL.
-    :return: The Sonetel account ID.
+    :param return_only_accountid: Optional. Returns only the account ID string if set to true. Else, returns the complete JSON response.
+    :return: A dict with the the Sonetel account details.
     """
 
-    if token is None:
-        raise ValueError(token)
-    elif base_url is None:
-        raise ValueError(base_url)
-    else:
-        url = f"{base_url}/account/"
-        header = {
-                "Authorization": "Bearer " + token,
-                "Content-Type": "application/json"
-        }
-        r = requests.get(url=url, headers=header)
-        if r.status_code == requests.codes.ok:
-            response = r.json()
-            return response['response']['account_id']
+    # Prepare the request Header
+    header = {
+            "Authorization": "Bearer " + token,
+            "Content-Type": "application/json"
+    }
+
+    # Send the request
+    r = requests.get(
+        url=f"{base_url}/account/",
+        headers=header)
+
+    # Check the response and handle accordingly
+    if r.status_code == requests.codes.ok:
+        response = r.json()
+        if not return_only_accountid:
+            return response
         else:
-            print(r.json())
-            r.raise_for_status()
+            return response['response']['account_id']
+    else:
+        print(r.json())
+        r.raise_for_status()
+
 
 # Get the current prepaid balance
-def get_balance(token: str = None, currency: bool = False, base_url: str = None) -> str:
+def get_balance(token: str, base_url: str = 'https://public-api.sonetel.com', currency: bool = False) -> str:
     """
-    Get the prepaid account balance. Example, '3.74 USD' or '3.74'
+    Get the prepaid account balance. Example, '3.74 USD' or '3.74'.
+    More information is available at https://docs.sonetel.com/docs/sonetel-documentation/b3A6MTUyNTkwNTM-get-your-account-information
 
     :param token: API access token generated using the token() function.
-    :param currency: Boolean value to specify if the currency should be returned or not. False by default.
     :param base_url: The API base URL.
+    :param currency: Optional. Flag to specify if the currency should be returned or not. Defaults to False
     :return: The current prepaid balance.
     """
 
-    if token is None:
-        raise ValueError(token)
-    else:
-        url = f"{base_url}/account/"
-        header = {
-            "Authorization": "Bearer " + token,
-            "Content-Type": "application/json"
-        }
-        r = requests.get(url=url, headers=header)
-        if r.status_code == requests.codes.ok:
-            response = r.json()
-            if currency is False:
-                return response['response']['credit_balance']
-            else:
-                return response['response']['credit_balance'] + ' ' + response['response']['currency']
+    # Prepare the request Header
+    header = {
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/json"
+    }
+
+    # Send the request
+    r = requests.get(
+        url=f"{base_url}/account/",
+        headers=header)
+
+    # Check the response and handle accordingly
+    if r.status_code == requests.codes.ok:
+        response = r.json()
+        if currency is False:
+            return response['response']['credit_balance']
         else:
-            print(r.json())
-            r.raise_for_status()
+            return response['response']['credit_balance'] + ' ' + response['response']['currency']
+    else:
+        print(r.json())
+        r.raise_for_status()
 
 # Fetch details of all users in account
-def get_all_users():
-    pass
+def get_all_users(token: str, accountid: str, base_url: str = 'https://public-api.sonetel.com') -> list:
+    """
+    Get a list of all the users in the Sonetel account along with their settings such as title, email, password status, etc.
+    More information is available at https://docs.sonetel.com/docs/sonetel-documentation/b3A6MTY4MzEyMDQ-list-all-users
+
+    :param token: API access token generated using the token() function.
+    :param accountid: Your Sonetel account ID.
+    :param base_url: The API base URL.
+    :return: Returns a list containing the user information
+    """
+
+    # Prepare the request Header
+    request_header = {
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/json"
+    }
+
+    # Send the request
+    r = requests.get(
+        url=f'{base_url}/account/{accountid}/user/',
+        headers=request_header
+    )
+    # Check the response and handle accordingly
+    if r.status_code == requests.codes.ok:
+        response = r.json()
+        return response['response']
+    else:
+        print(r.json())
+        r.raise_for_status()
